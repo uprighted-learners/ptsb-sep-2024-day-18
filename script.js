9a04345b6749e1f07d579a133259db38015e4fdc
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-24 17:26:21 -0500

    pseudocoded

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+//an array of movies not watched yet
+
+//a second arrays of movies already watched
+
+//a function to add movies to not yet watched list
+
+//a function to move movies to watched list
+
+//a function to view movies on watch list
+
+//a function to view ALL movies (both lists)
+
+//main menu function =)
